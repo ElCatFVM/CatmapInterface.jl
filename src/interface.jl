aad1bfd2fcb4f636317299c7470340f9d1bf47cd
bmchun722 <bmchun722@gmail.com>
2025-10-12 03:09:57 +0900

    without warning message for beta_mode=:none

--- src/interface.jl
+++ src/interface.jl
@@ -450,7 +450,6 @@ function parse_catmap_input(input_file_path::AbstractString)
     py"
 $$input
 "
-    pyglobals = pybuiltin("globals")
     
     beta = 
     try
@@ -489,13 +488,7 @@ $$input
     gas_thermo_mode             = Symbol(py"gas_thermo_mode")
     adsorbate_thermo_mode       = Symbol(py"adsorbate_thermo_mode")
     electrochemical_thermo_mode = Symbol(py"electrochemical_thermo_mode")
-
-    if haskey(pyglobals, "beta_mode")
-        beta_mode = Symbol(py"beta_mode")
-    else
-        beta_mode = :none
-        @warn "beta mode not specified. Defaulting to $(beta_mode)."
-    end
+    beta_mode = Symbol(py"beta_mode")
     adsorbate_interaction_params = _get_adsorbate_interaction_params()
     
     species_list = specieslist(reactions, species_definitions, energy_table, surface_name; electrochemical_thermo_mode)

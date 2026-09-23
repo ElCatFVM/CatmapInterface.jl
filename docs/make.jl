f850c1bab31552f54e2dd097cfadc33054989412
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-09-23 18:35:30 +0200

    Add notebook to documentation Restructure use of notebook environment: run them in the test  env in tests

--- docs/make.jl
+++ docs/make.jl
@@ -1,43 +1,50 @@
 using Documenter
 using DocumenterMermaid
+using ExampleJuggler, PlutoStaticHTML
+using CairoMakie
 using CatmapInterface
 using Catalyst
-using PlutoStaticHTML
 using Pkg
 
 
 function mkdocs()
-    # TODO: add notebooks with the help of ExampleJuggler 
 
-    DocMeta.setdocmeta!(CatmapInterface, :DocTestSetup, :(using CatmapInterface, Catalyst); recursive=true)
+    ExampleJuggler.verbose!(true)
 
-    makedocs(
-        sitename    = "CatmapInterface.jl",
-        modules     = [CatmapInterface],
-        format      = Documenter.HTML(
-            size_threshold  = nothing,
-            mathengine      = MathJax3(
+    cleanexamples()
+    notebookdir = joinpath(@__DIR__, "..", "notebooks")
+    notebooks = ["CO2 reduction" => "CO2R.jl"]
+    notebook_examples = @docplutonotebooks(notebookdir, notebooks, iframe = false, append_build_context = false)
+
+    DocMeta.setdocmeta!(CatmapInterface, :DocTestSetup, :(using CatmapInterface, Catalyst); recursive = true)
+
+    return makedocs(
+        sitename = "CatmapInterface.jl",
+        modules = [CatmapInterface],
+        format = Documenter.HTML(
+            size_threshold = nothing,
+            mathengine = MathJax3(
                 Dict(
                     :tex => Dict(
-                        "inlineMath" => [["\$","\$"], ["\\(","\\)"]],
+                        "inlineMath" => [["\$", "\$"], ["\\(", "\\)"]],
                         "tags" => "ams",
                         "packages" => ["base", "ams", "autoload", "mhchem"],
                     )
                 )
             )
         ),
-        clean       = false,
-        doctest     = false,
-        draft       = false,
-        authors     = "S. Maaß",
-        repo        = "https://github.com/ElCatFVM/CatmapInterface.jl",
-        pages       = [
-            "Home"      => "index.md",
-            "Guide Outline"     => "guide.md",
-            "Public API"        => "public.md",
-            "Information flow"  => "package-flow.md",
-            "Internal API"     => "internal.md",
-            #"Notebooks" => notebooks,
+        clean = false,
+        doctest = false,
+        draft = false,
+        authors = "S. Maaß",
+        repo = "https://github.com/ElCatFVM/CatmapInterface.jl",
+        pages = [
+            "Home" => "index.md",
+            "Guide Outline" => "guide.md",
+            "Public API" => "public.md",
+            "Information flow" => "package-flow.md",
+            "Internal API" => "internal.md",
+            "Notebooks" => notebook_examples,
         ]
     )
 end
